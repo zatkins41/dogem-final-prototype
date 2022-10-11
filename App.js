e8fb4542883165588a14f05b907366a90507112d
Melissa <melissainiestrav@gmail.com>
2022-10-10 21:27:48 -0400

    Logout Button created

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button, View} from 'react-native'
 
 import LoginScreen from './Screens/Login'
 import HomeScreen from './Screens/Home'
@@ -22,7 +23,21 @@ const MyStack = () => {
                 <Stack.Screen
                     name="Login"
                     component={LoginScreen}
-                    options={{ title: 'Home' }}
+                    // options={{ title: 'Home' }}
+                    options={({navigation}) => ({
+                        title:'Home',
+                        headerStyle: {
+                          backgroundColor: '',
+                        },
+                        headerRight: () => (
+                          <View style={{marginHorizontal: -10, flexDirection: "row"}}>
+                           <Button
+                             title="Logout" //Header Button to Logout
+                             onPress={() => navigation.navigate('Home')}   
+                            />
+                          </View>
+                        )  
+                       })}
                 />
                 <Stack.Screen name="Dogem" component={DogemScreen} options={{title:'Dogem'} }/>
                 <Stack.Screen name="Contact" component={ContactScreen} options={{title:'Contact'} }/>
